a31909b465616323b925094fa3e737fd818b61ff
xiaolong <zxl3514@126.com>
2015-11-18 15:38:23 +0800

    xiaolong

--- ios/MaiYa/MaiYa/SearchViewController.m
+++ ios/MaiYa/MaiYa/SearchViewController.m
@@ -11,8 +11,8 @@
 #import "MyZoneViewController.h"
 
 @interface SearchViewController () <UISearchBarDelegate, UISearchDisplayDelegate, UITableViewDelegate, UITableViewDataSource>
-@property (strong, nonatomic) NSArray *searchResults;
-
+@property (strong, nonatomic) NSMutableArray *searchResults;
+@property (copy, nonatomic) NSString *selectedMasterId;
 @end
 
 @implementation SearchViewController
@@ -21,7 +21,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.searchResults = [NSArray new];
+    self.searchResults = [NSMutableArray new];
     
     self.searchDisplayController.searchBar.tintColor = [UIColor whiteColor];
     
@@ -37,24 +37,63 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - NetWorking
+- (void)getUserListByKeyWordStr:(NSString *)keyWordStr {
+    NSString *uid = [UserConfigManager shareManager].userInfo.uidStr;
+    
+    NSMutableDictionary *dic = [NSMutableDictionary new];
+    [dic setObject:uid forKey:@"uid"];
+    
+    if (keyWordStr.isValid) {
+        [dic setObject:keyWordStr forKey:@"fuzzy"];
+    }
+    
+    [[NetworkingManager shareManager] networkingWithGetMethodPath:@"userList" params:dic success:^(id responseObject) {
+        NSArray *resArr = [responseObject objectForKey:@"res"];
+        for (NSDictionary *dic in resArr) {
+            UserZoneModel *model = [[UserZoneModel alloc] initWithDic:dic];
+            UserZoneViewModel *viewModel = [[UserZoneViewModel alloc] initWithUserZoneModel:model];
+            [self.searchResults addObject:viewModel];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (0 == self.searchResults.count) {
+                UITableView *tableView1 = self.searchDisplayController.searchResultsTableView;
+                for( UIView *subview in tableView1.subviews ) {
+                    if( [subview class] == [UILabel class] ) {
+                        UILabel *lbl = (UILabel*)subview; // sv changed to subview.
+                        lbl.text = @"没有结果";
+                    }
+                }
+            } else {
+                [self.searchDisplayController.searchResultsTableView reloadData];
+            }
+        });
+    }];
+}
+
 #pragma mark - Navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    
     if ([segue.identifier isEqualToString:@"ShowMasterZone"]) {
         MyZoneViewController *contrller = segue.destinationViewController;
         contrller.type = ZoneViewControllerTypeOfOther;
+        
+        NSString *uid = [UserConfigManager shareManager].userInfo.uidStr;
+        contrller.cidStr = self.selectedMasterId;
+        contrller.oidStr = uid;
     }
 }
 
 #pragma mark - UITableViewDataSource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 20;
+    return self.searchResults.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     MasterCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MasterCell"];
+    
+    UserZoneViewModel *viewModel = [self.searchResults objectAtIndex:indexPath.row];
+    [cell layoutMasterCellSubviewsByUserZoneViewModel:viewModel];
     
     return cell;
 }
@@ -62,21 +101,18 @@
 #pragma mark - UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    UserZoneViewModel *viewModel = [self.searchResults objectAtIndex:indexPath.row];
+    self.selectedMasterId = viewModel.uidStr;
+    
     [self performSegueWithIdentifier:@"ShowMasterZone" sender:self];
 }
 
 
 #pragma mark - UISearchDisplayDelegate
 -(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString {
-    if (self.searchResults.count == 0) {
-        UITableView *tableView1 = self.searchDisplayController.searchResultsTableView;
-        for( UIView *subview in tableView1.subviews ) {
-            if( [subview class] == [UILabel class] ) {
-                UILabel *lbl = (UILabel*)subview; // sv changed to subview.
-                lbl.text = @"没有结果";
-            }
-        }
-    }
+    [self getUserListByKeyWordStr:searchString];
+    
     return YES;
 }
 
