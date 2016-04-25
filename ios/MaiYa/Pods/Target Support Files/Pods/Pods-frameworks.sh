c5a76e00c2a2755af74f33044f9bfa1422a5a590
xiaolong <zxl3514@126.com>
2016-04-25 10:11:53 +0800

    xiaolong

--- ios/MaiYa/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ ios/MaiYa/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -82,3 +82,16 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/AFNetworking.framework"
+  install_framework "Pods/PureLayout.framework"
+  install_framework "Pods/ReactiveCocoa.framework"
+  install_framework "Pods/UICountingLabel.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/AFNetworking.framework"
+  install_framework "Pods/PureLayout.framework"
+  install_framework "Pods/ReactiveCocoa.framework"
+  install_framework "Pods/UICountingLabel.framework"
+fi
